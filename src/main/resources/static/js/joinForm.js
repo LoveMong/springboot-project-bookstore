26e7e0c19b8b2136650592dab9c7ab44b34d4eb1
LeeMong <izpzgi2019@gmail.com>
2022-09-07 21:26:27 +0900

    회원가입 화면 수정

--- src/main/resources/static/js/joinForm.js
+++ src/main/resources/static/js/joinForm.js
@@ -16,157 +16,154 @@ let password_pattern = /^[a-zA-z0-9]{4,12}$/;
 let email_pattern = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
 
 
-$(document).ready(function(){
+// $(document).ready(function(){
+//
+// // 아이디 생성 시 중복 검사
+//     $('#idCheck').click(function(){
+//
+//         id = document.getElementById("member_id").value;
+//
+//         if (id === "") {
+//             setMessage_Join("아이디를 입력해주세요.", jrm.member_id)
+//             jrm.member_id.focus();
+//         }
+//         else if (id.length < 4 || id.length > 12) {
+//             setMessage_Join("아이디 길이는 4 ~ 12 사이로 입력해주세요.", jrm.member_id)
+//             jrm.member_id.focus();
+//         }
+//         else if (id.replace(blank_pattern, '') === "") {
+//             setMessage_Join("아이디에 공백이 포함되었습니다.", jrm.member_id)
+//             jrm.member_id.focus();
+//         }
+//         else if (blank_pattern_S.test(id) === true) {
+//             setMessage_Join("아이디에 공백이 포함되었습니다.", jrm.member_id)
+//             jrm.member_id.focus();
+//         }
+//         else if (special_pattern.test(id) === true) {
+//             setMessage_Join("아이디에 특수문자가 포함되었습니다.", jrm.member_id)
+//             jrm.member_id.focus();
+//         }
+//         else if (id.search(/\W|\s/g) > -1) {
+//             setMessage_Join("아이디에 공백 또는 특수문자가 포함되었습니다.", jrm.member_id)
+//             jrm.member_id.focus();
+//         } else {
+//             $.ajax({
+//                 type : 'get', // 요청 메서드
+//                 url : '/register/checkId', // 요청 URI
+//                 data : {member_id : id}, // 서버로 전송할 데이터
+//                 success : function (result) {
+//                     if(result === 1) {
+//                         setMessage_Join("사용가능한 아이디입니다.", jrm.member_id);
+//                         document.getElementById("idCheck").value = "Check Completed";
+//                     } else {
+//                         setMessage_Join("이미 사용중인 아이디입니다.", jrm.member_id);
+//                     }
+//
+//                 },
+//             })
+//             // window.open을 이용한 idCheck version
+//             // let url = "/register/checkId?member_id=" + document.jrm.member_id.value;
+//             // let popupX = (window.screen.width / 2) - (450 / 2);
+//             // popupX += window.screenLeft;
+//             // let popupY = (window.screen.height / 2) - (400 / 2);
+//             // let option = "toolbar=no, menubar=no, scrollbars=yes, resizable=no, width=450, height=400, left=" + popupX + ", top=" + popupY;
+//             // window.open(url, "_blank", option);
+//         }
+//
+//
+//     });
 
-// login_register 폼 변경 TAP
-    $('#login-form-link').click(function(e) {
-        $("#login-form").delay(100).fadeIn(100);
-        $("#register-form").fadeOut(100);
-        $('#register-form-link').removeClass('active');
-        $(this).addClass('active');
-        e.preventDefault();
-    });
-    $('#register-form-link').click(function(e) {
-        $("#register-form").delay(100).fadeIn(100);
-        $("#login-form").fadeOut(100);
-        $('#login-form-link').removeClass('active');
-        $(this).addClass('active');
-        e.preventDefault();
-    });
-
-// 아이디 생성 시 중복 검사
-    $('#idCheck').click(function(){
-
-        id = document.getElementById("member_id").value;
-
-        if (id === "") {
-            setMessage_Join("아이디를 입력해주세요.", jrm.member_id)
-            jrm.member_id.focus();
-        }
-        else if (id.length < 4 || id.length > 12) {
-            setMessage_Join("아이디 길이는 4 ~ 12 사이로 입력해주세요.", jrm.member_id)
-            jrm.member_id.focus();
-        }
-        else if (id.replace(blank_pattern, '') === "") {
-            setMessage_Join("아이디에 공백이 포함되었습니다.", jrm.member_id)
-            jrm.member_id.focus();
-        }
-        else if (blank_pattern_S.test(id) === true) {
-            setMessage_Join("아이디에 공백이 포함되었습니다.", jrm.member_id)
-            jrm.member_id.focus();
-        }
-        else if (special_pattern.test(id) === true) {
-            setMessage_Join("아이디에 특수문자가 포함되었습니다.", jrm.member_id)
-            jrm.member_id.focus();
-        }
-        else if (id.search(/\W|\s/g) > -1) {
-            setMessage_Join("아이디에 공백 또는 특수문자가 포함되었습니다.", jrm.member_id)
-            jrm.member_id.focus();
-        } else {
-            $.ajax({
-                type : 'get', // 요청 메서드
-                url : '/register/checkId', // 요청 URI
-                data : {member_id : id}, // 서버로 전송할 데이터
-                success : function (result) {
-                    if(result === 1) {
-                        setMessage_Join("사용가능한 아이디입니다.", jrm.member_id);
-                        document.getElementById("idCheck").value = "Check Completed";
-                    } else {
-                        setMessage_Join("이미 사용중인 아이디입니다.", jrm.member_id);
-                    }
-
-                },
-            })
-            // window.open을 이용한 idCheck version
-            // let url = "/register/checkId?member_id=" + document.jrm.member_id.value;
-            // let popupX = (window.screen.width / 2) - (450 / 2);
-            // popupX += window.screenLeft;
-            // let popupY = (window.screen.height / 2) - (400 / 2);
-            // let option = "toolbar=no, menubar=no, scrollbars=yes, resizable=no, width=450, height=400, left=" + popupX + ", top=" + popupY;
-            // window.open(url, "_blank", option);
-        }
+//     // 아이디 입력칸 누르면 idCehck 다시 유도
+//     document.getElementById("member_id").onclick = function () {
+//         document.getElementById("idCheck").value = "Check ID";
+//     }
+// });
 
 
-    });
-
-    // 아이디 입력칸 누르면 idCehck 다시 유도
-    document.getElementById("member_id").onclick = function () {
-        document.getElementById("idCheck").value = "Check ID";
-    }
-
-});
-
-// Login 유효성 검사
-function loginCheck() {
-
-    if (document.frm.memberId.value.length === 0) {
-        setMessage_Login('아이디를 입력해주세요.', frm.member_id)
-        return false;
-    }
-    if (document.frm.memberPwd.value === "") {
-        setMessage_Login('패스워드를 입력해주세요.', frm.memberPwd)
-        return false;
-    }
-
-    return true;
-
-}
-
-function setMessage_Login(message, element){
-    document.getElementById("message").innerHTML = `<i class="fa fa-exclamation-circle"> ${message}</i>`;
-    if(element) {
-        element.select();
-    }
-}
+// // Login 유효성 검사
+// function loginCheck() {
+//
+//     if (document.frm.memberId.value.length === 0) {
+//         setMessage_Login('아이디를 입력해주세요.', frm.member_id)
+//         return false;
+//     }
+//     if (document.frm.memberPwd.value === "") {
+//         setMessage_Login('패스워드를 입력해주세요.', frm.memberPwd)
+//         return false;
+//     }
+//
+//     return true;
+//
+// }
+//
+// function setMessage_Login(message, element){
+//     document.getElementById("message").innerHTML = `<i class="fa fa-exclamation-circle"> ${message}</i>`;
+//     if(element) {
+//         element.select();
+//     }
+// }
 
 
 function joinCheck() {
 
-    id = document.getElementById("member_id").value;
-    jpwd = document.getElementById("member_jpwd").value;
-    cpwd = document.getElementById("member_cpwd").value;
-    name = document.getElementById("member_name").value;
-    email = document.getElementById("member_email").value;
+    email = document.getElementById("memberEmail").value;
+    name = document.getElementById("memberName").value;
+    pwd = document.getElementById("memberPassword").value;
+    cpwd = document.getElementById("memberPassword2").value;
 
-    if(document.getElementById("idCheck").value === "Check ID") {
-        setMessage_Join("아이디 확인이 필요합니다.", jrm.member_id);
-        jrm.member_id.focus();
-    }
-    else if (name === "") {
-        setMessage_Join("이름을 입력해주세요.", jrm.member_name);
+    var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
+
+    if (name === "") {
+        setMessage_Join("이름을 입력해주세요.", signFrm.memeberName);
     }
     else if (name.length < 2 || name.length > 10) {
-        setMessage_Join("이름은 2 ~ 10 사이로 입력해주세요.", jrm.member_name);
+        setMessage_Join("이름은 2 ~ 10 사이로 입력해주세요.", signFrm.memeberName);
     }
+    else if (name.replace(blank_pattern, '') === "") {
+        setMessage_Join("이름에 공백이 포함되었습니다.", signFrm.memeberName);
+    }
+    else if (blank_pattern_S.test(name) === true) {
+        setMessage_Join("이름에 공백이 포함되었습니다.", signFrm.memeberName);
+    }
+    // else if (special_pattern.test(name) === true) {
+    //     setMessage_Join("이름에 특수문자가 포함되었습니다.", signFrm.memeberName);
+    // }
+    // else if (name.search(/\W|\s/g) > -1) {
+    //     setMessage_Join("이름에 공백 또는 특수문자가 포함되었습니다.", signFrm.memeberName);
+    // }
     else if (email === "") {
-        setMessage_Join("이메일을 입력해주세요.", jrm.member_email);
+        setMessage_Join("이메일을 입력해주세요.", signFrm.memberEmail);
     }
     else if (email_pattern.test(email) === false) {
-        setMessage_Join("이메일 형식이 아닙니다.", jrm.member_email);
+        setMessage_Join("이메일 형식이 아닙니다.", signFrm.memberEmail);
     }
-    else if (jpwd === "") {
-        setMessage_Join("비밀번호를 입력해주세요.", jrm.member_jpwd);
+    else if (pwd === "") {
+        setMessage_Join("비밀번호를 입력해주세요.", signFrm.memberPassword);
     }
-    else if (password_pattern.test(jpwd) === false) {
-        setMessage_Join("비밀번호는 영문 대소문자와 숫자 4~12자리로 입력해주세요.", jrm.member_jpwd);
+    else if (password_pattern.test(pwd) === false) {
+        setMessage_Join("비밀번호는 영문 대소문자와 숫자 4~12자리로 입력해주세요.", signFrm.memberPassword);
     }
     else if (cpwd === "") {
-        setMessage_Join("비밀번호를 확인해주세요.", jrm.member_cpwd);
+        setMessage_Join("비밀번호를 확인해주세요.", signFrm.memberPassword2);
     }
-    else if (jpwd !== cpwd) {
-        setMessage_Join("비밀번호가 일치하지 않습니다.", jrm.member_cpwd);
+    else if (pwd !== cpwd) {
+        setMessage_Join("비밀번호가 일치하지 않습니다.", signFrm.memberPassword2);
     } else {
         $.ajax({
             type : 'post', // 요청 메서드
-            url : '/register/save', // 요청 URI
-            data : {member_id : id,
-                    member_name : name,
-                    member_pwd : jpwd,
-                    member_email : email}, // 서버로 전송할 데이터
-            success : function (result) {
-                if(result === 1) {
+            url : '/account/sign-up', // 요청 URI
+            beforeSend : function (xhr){
+                xhr.setRequestHeader(header, token);
+            },
+            data : {memberName : name,
+                    memberEmail : email,
+                    memberPassword : pwd
+                    }, // 서버로 전송할 데이터
+            success : function (joinSuccessResult) {
+                if(joinSuccessResult === 1) {
                     alert("회원가입에 성공했습니다. 다시 로그인해주세요.");
-                    location.reload();
+                    location.href = '/account/sign-in';
                 } else {
                     alert("회원가입에 실패했습니다. 다시 시도해주세요.");
                 }
@@ -190,7 +187,7 @@ function editCheck() {
 
     id = document.getElementById("member_id").value;
     name = document.getElementById("member_name").value;
-    jpwd = document.getElementById("member_pwd").value;
+    pwd = document.getElementById("member_pwd").value;
     cpwd = document.getElementById("member_cpwd").value;
     email = document.getElementById("member_email").value;
 
@@ -201,16 +198,16 @@ function editCheck() {
     else if (email_pattern.test(email) === false) {
         setMessage_edit("이메일 형식이 아닙니다.", prm.member_email);
     }
-    else if (jpwd === "") {
-        setMessage_edit("비밀번호를 입력해주세요.", prm.member_jpwd);
+    else if (pwd === "") {
+        setMessage_edit("비밀번호를 입력해주세요.", prm.member_pwd);
     }
-    else if (password_pattern.test(jpwd) === false) {
-        setMessage_edit("비밀번호는 영문 대소문자와 숫자 4~12자리로 입력해주세요.", prm.member_jpwd);
+    else if (password_pattern.test(pwd) === false) {
+        setMessage_edit("비밀번호는 영문 대소문자와 숫자 4~12자리로 입력해주세요.", prm.member_pwd);
     }
     else if (cpwd === "") {
         setMessage_edit("비밀번호를 확인해주세요.", prm.member_cpwd);
     }
-    else if (jpwd !== cpwd) {
+    else if (pwd !== cpwd) {
         setMessage_edit("비밀번호가 일치하지 않습니다.", prm.member_cpwd);
     } else {
         $.ajax({
@@ -218,7 +215,7 @@ function editCheck() {
             url : '/login/edit', // 요청 URI
             data : {member_id : id,
                 member_name : name,
-                member_pwd : jpwd,
+                member_pwd : pwd,
                 member_email : email}, // 서버로 전송할 데이터
             success : function (result) {
                 if(result === 1) {
--- src/main/webapp/WEB-INF/views/member/joinForm.jsp
+++ src/main/webapp/WEB-INF/views/member/joinForm.jsp
@@ -1,18 +1,23 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html lang="ko">
 
 <head>
-<script
-	src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script	src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- ajax 통신을 위한 meta tag -->
+<meta name="_csrf" content="${_csrf.token}">
+<meta name="_csrf_header" content="${_csrf.headerName}">
 <title>BookStore</title>
 <!--  CSS Link -->
 <link rel="stylesheet" href="/css/member/joinForm.css">
 <!-- Bootstrap CSS Link -->
-<link rel="stylesheet" href="/css/bootstraps/css/bootstrap.css">
+<link rel="stylesheet"  href="/css/bootstraps/css/bootstrap.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css"/>
+<script type="text/javascript" src="/js/joinForm.js"></script>
 </head>
 <body>
 
@@ -22,7 +27,6 @@
 				<!--회원가입섹션-->
 				<section class="login">
 					<article class="login__form__container">
-	
 						<!--회원가입 폼-->
 						<div class="login__form">
 							<!--로고-->
@@ -31,17 +35,15 @@
 	            			</div>
 							<!--로고end-->
 							<!--회원가입 인풋-->
-							<form class="login__input" action="/join" method="post" id="signFrm" name="signFrm">
-
+							<form class="login__input" action="" method="" id="signFrm" name="signFrm">
 								<div id="main_name" class="form-floating mb-4" style="width: 330px">
-									<input type="text" name="user_name" class="form-control" id="user_name" placeholder="Name" required="required">
-									<label for="user_name">이름</label>
+									<input type="text" name="memberName" class="form-control" id="memberName" placeholder="Name" required="required">
+									<label for="memberName">이름</label>
 								</div>
-
 								<div class="form-floating mb-4">
 									<div id="mail_sign" class="form-floating mb-4" style="width: 330px">
-										<input type="email" name="user_email" class="form-control" id="user_email" placeholder="E-mail" required="required">
-										<label for="user_email">이메일 </label>
+										<input type="email" name="memberEmail" class="form-control" id="memberEmail" placeholder="E-mail" required="required">
+										<label for="memberEmail">이메일 </label>
 									</div>
 									<div><button type="button" id="mail_check" value="이메일인증받기" class="btn btn-success">인증메일 발송</button></div>
 								</div>
@@ -51,20 +53,19 @@
 									<button type="button" id="mail_num_sign" value="인증번호확인" class="btn btn-success">인증번호 확인</button>
 								</div>
 								<div id="main_pw" class="form-floating mb-4" style="width: 330px">
-									<input type="password" name="user_pw" class="form-control" id="user_pw" placeholder="Password" required="required">
-									<label for="user_pw">비밀번호</label>
+									<input type="password" name="memberPassword" class="form-control" id="memberPassword" placeholder="Password" required="required">
+									<label for="memberPassword">비밀번호</label>
 								</div>
 								<div id="main_pw2" class="form-floating mb-4" style="width: 330px">
-									<input type="password" name="user_pw2" class="form-control" id="user_pw2" placeholder="Check" required="required">
-									<label for="user_pw2">비밀번호 확인</label>
+									<input type="password" name="memberPassword2" class="form-control" id="memberPassword2" placeholder="Check" required="required">
+									<label for="memberPassword2">비밀번호 확인</label>
 								</div>
-								<br />
-
-								<br />
-								<br/>
-								<!-- <input type="button" id="signUp" value="회원가입"> -->
-								<button type="button" class="btn btn-primary" id="signUp" value="회원가입" onclick="check()">회원가입</button>
-								
+								<div class="form-group text-center" id="message_join">
+									<c:if test="${not empty param.joinMessage}">
+										<i class="fa fa-exclamation-circle"> ${URLDecoder.decode(param.joinMessage, "utf-8")}</i>
+									</c:if>
+								</div>
+								<button type="button" class="btn btn-primary" id="signUp" value="회원가입" onclick="joinCheck()">회원가입</button>
 							</form>
 							<!--회원가입 인풋end-->
 						</div>
@@ -106,122 +107,122 @@
 	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 </body>
 
-<script type="text/javascript">
-	$(document).ready(function(e) {		
-
-		var idx = false;
-		var check_main_num ;
-		var check_main_num_ck;
-		var check_same_mail;
-	
-		
-		$('#signUp').click(function() {
-			if ($.trim($('#user_id').val()) == "") {
-				alert("아이디를 입력해주세.");
-				$('#user_id').focus();
-				return;
-			} else if ($.trim($('#user_pw').val()) == '') {
-				alert("패스워드를 입력해주세.");
-				$('#user_pw').focus();
-				return;
-			} else if ($.trim($('#user_phone').val()) == '') {
-				alert("핸드폰 번호를 입력해주세요.");
-				$('#user_phone').focus();
-				return;
-			} else if (check_main_num_ck == '' || check_main_num_ck == null) {
-				alert("이메일 인증을해주세요.");
-				$('#user_mail').focus();
-				return;
-			} else if ($.trim($('#user_name').val()) == '') {
-				alert("이름을 입력해주세요.");
-				$('#user_name').focus();
-				return;
-			} else if ($.trim($('#user_pw2').val()) == '') {
-				alert("비밀번호를 확인해주세요.");
-				$('#user_pw2').focus();
-				return;				
-			} else if ($.trim($('#user_pw').val()) != $.trim($('#user_pw2').val())) {
-				alert("비밀번호를 확인해주세요.");
-				$('#user_pw2').focus();
-				return;				
-			} else if ($("#user_email").val() != check_same_mail ) {
-				alert("인증한 메일과 현재 이메일이 같지 않습니다 확인 부탁드립니다.");
-				$('#user_mail').focus();
-				return;
-			} 
-			
-			if (idx == false) {
-				alert("아이디 중복체크를 해주세요.");
-				return;
-			} else {
-				$('#signFrm').submit();
-			}
-		});
-
-		$('#check').click(function() {
-			$.ajax({
-				url : "${pageContext.request.contextPath}/idCheck",
-				type : "GET",
-				data : {
-					"user_id" : $('#user_id').val()
-				},
-				success : function(data) {
-					if (data == 0 && $.trim($('#user_id').val()) != '') {
-						idx = true;
-						alert("사용 가능한 아이디 입니다.")
-					} else {
-						alert("사용 불가능한 아이디 입니다.")
-					}
-				},
-				error : function() {
-					alert("서버에러");
-				}
-			});
-		});
-		
-
-
-
-	/* 인증번호 이메일 전송 */
-	$("#mail_check").click(function(){
-		var email = $("#user_email").val();      
-		var cehckBox = $("#mail_sign");        // 인증번호 입력란
-		var boxWrap = $(".mail_check_input_box");   // 인증번호 입력란 박스
-		check_same_mail = $("#user_email").val();
-		// $("#mail_check").slideUp();
-   		$("#mail_sign_ch").slideDown("slow");
-
-		$.ajax({
-	        
-	        type : "GET",
-	        url : "${pageContext.request.contextPath}/emailCheck?email=" + email ,
-	       	success:function(data){
-	       		//console.log("data : " + data);
-	       		
-	       		check_main_num = data;
-	       	}
-	                
-	    });
-	    
-	    
-	});
-	
-	/* 인증번호 이메일 전송 */
-	$("#mail_num_sign").click(function(){
-		
-		if(check_main_num == $("#user_mail_check").val()) {
-			alert("인증완료");
-			check_main_num_ck = "mail_check";
-		} else {
-			alert("인증번호를 확인하세요.");
-			$('#user_mail_check').focus();		
-			
-		}
-		
-	});
-
-
-});
-	
-</script>
 </html>
+<%--<script type="text/javascript">--%>
+<%--	$(document).ready(function(e) {--%>
+
+<%--		var idx = false;--%>
+<%--		var check_main_num ;--%>
+<%--		var check_main_num_ck;--%>
+<%--		var check_same_mail;--%>
+
+
+<%--		$('#signUp').click(function() {--%>
+<%--			if ($.trim($('#user_id').val()) == "") {--%>
+<%--				alert("아이디를 입력해주세.");--%>
+<%--				$('#user_id').focus();--%>
+<%--				return;--%>
+<%--			} else if ($.trim($('#user_pw').val()) == '') {--%>
+<%--				alert("패스워드를 입력해주세.");--%>
+<%--				$('#user_pw').focus();--%>
+<%--				return;--%>
+<%--			} else if ($.trim($('#user_phone').val()) == '') {--%>
+<%--				alert("핸드폰 번호를 입력해주세요.");--%>
+<%--				$('#user_phone').focus();--%>
+<%--				return;--%>
+<%--			} else if (check_main_num_ck == '' || check_main_num_ck == null) {--%>
+<%--				alert("이메일 인증을해주세요.");--%>
+<%--				$('#user_mail').focus();--%>
+<%--				return;--%>
+<%--			} else if ($.trim($('#user_name').val()) == '') {--%>
+<%--				alert("이름을 입력해주세요.");--%>
+<%--				$('#user_name').focus();--%>
+<%--				return;--%>
+<%--			} else if ($.trim($('#user_pw2').val()) == '') {--%>
+<%--				alert("비밀번호를 확인해주세요.");--%>
+<%--				$('#user_pw2').focus();--%>
+<%--				return;--%>
+<%--			} else if ($.trim($('#user_pw').val()) != $.trim($('#user_pw2').val())) {--%>
+<%--				alert("비밀번호를 확인해주세요.");--%>
+<%--				$('#user_pw2').focus();--%>
+<%--				return;--%>
+<%--			} else if ($("#user_email").val() != check_same_mail ) {--%>
+<%--				alert("인증한 메일과 현재 이메일이 같지 않습니다 확인 부탁드립니다.");--%>
+<%--				$('#user_mail').focus();--%>
+<%--				return;--%>
+<%--			}--%>
+
+<%--			if (idx == false) {--%>
+<%--				alert("아이디 중복체크를 해주세요.");--%>
+<%--				return;--%>
+<%--			} else {--%>
+<%--				$('#signFrm').submit();--%>
+<%--			}--%>
+<%--		});--%>
+
+<%--		$('#check').click(function() {--%>
+<%--			$.ajax({--%>
+<%--				url : "${pageContext.request.contextPath}/idCheck",--%>
+<%--				type : "GET",--%>
+<%--				data : {--%>
+<%--					"user_id" : $('#user_id').val()--%>
+<%--				},--%>
+<%--				success : function(data) {--%>
+<%--					if (data == 0 && $.trim($('#user_id').val()) != '') {--%>
+<%--						idx = true;--%>
+<%--						alert("사용 가능한 아이디 입니다.")--%>
+<%--					} else {--%>
+<%--						alert("사용 불가능한 아이디 입니다.")--%>
+<%--					}--%>
+<%--				},--%>
+<%--				error : function() {--%>
+<%--					alert("서버에러");--%>
+<%--				}--%>
+<%--			});--%>
+<%--		});--%>
+
+
+
+
+<%--		/* 인증번호 이메일 전송 */--%>
+<%--		$("#mail_check").click(function(){--%>
+<%--			var email = $("#user_email").val();--%>
+<%--			var cehckBox = $("#mail_sign");        // 인증번호 입력란--%>
+<%--			var boxWrap = $(".mail_check_input_box");   // 인증번호 입력란 박스--%>
+<%--			check_same_mail = $("#user_email").val();--%>
+<%--			// $("#mail_check").slideUp();--%>
+<%--			$("#mail_sign_ch").slideDown("slow");--%>
+
+<%--			$.ajax({--%>
+
+<%--				type : "GET",--%>
+<%--				url : "${pageContext.request.contextPath}/emailCheck?email=" + email ,--%>
+<%--				success:function(data){--%>
+<%--					//console.log("data : " + data);--%>
+
+<%--					check_main_num = data;--%>
+<%--				}--%>
+
+<%--			});--%>
+
+
+<%--		});--%>
+
+<%--		/* 인증번호 이메일 전송 */--%>
+<%--		$("#mail_num_sign").click(function(){--%>
+
+<%--			if(check_main_num == $("#user_mail_check").val()) {--%>
+<%--				alert("인증완료");--%>
+<%--				check_main_num_ck = "mail_check";--%>
+<%--			} else {--%>
+<%--				alert("인증번호를 확인하세요.");--%>
+<%--				$('#user_mail_check').focus();--%>
+
+<%--			}--%>
+
+<%--		});--%>
+
+
+<%--	});--%>
+
+<%--</script>--%>
