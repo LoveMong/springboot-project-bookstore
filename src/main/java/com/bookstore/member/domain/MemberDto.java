6dec02eab04750a83809fec5f9720a0ce9453e99
LeeMong <izpzgi2019@gmail.com>
2022-09-08 20:00:15 +0900

    memberDto 수정

--- src/main/java/com/bookstore/member/domain/MemberDto.java
+++ src/main/java/com/bookstore/member/domain/MemberDto.java
@@ -2,8 +2,12 @@ package com.bookstore.member.domain;
 
 
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import java.util.Date;
 
 @Builder
@@ -14,10 +18,16 @@ import java.util.Date;
 public class MemberDto {
 
     @NotBlank
+    @Length(min = 2, max = 10)
     private String memberName;
 
+    @NotEmpty
+    @Email
+    @Pattern(regexp = "\\w+@\\w+\\.\\w+(\\.\\w+)?", message = "이메일 형식이 아닙니다.")
     private String memberEmail;
 
+    @NotEmpty
+    @Length(min = 4, max = 12)
     private String memberPassword;
 
     private Date memberRegisterDate;
