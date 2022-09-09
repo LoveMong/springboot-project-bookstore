2e742f7cc7ae7033f69b3ac9b96010dc2e33f36b
LeeMong <izpzgi2019@gmail.com>
2022-09-09 09:04:37 +0900

    memberValidation 수정

--- src/main/java/com/bookstore/common/validation/MemberValidator.java
+++ src/main/java/com/bookstore/common/validation/MemberValidator.java
@@ -46,11 +46,6 @@ public class MemberValidator implements Validator {
         String memberPassword = memberDto.getMemberPassword();
 
 
-//      memberId가 null이거나 빈 문자열이면 memberId 필드에서 required 라는 error 코드 저장
-//      if (member_id == null || "".equals(member_id.trim())) {
-//         errors.rejectValue("member_id", "required", "id는 필수값 입니다.");
-//      }
-
 
 
         if (nameCheck(memberName)) {
