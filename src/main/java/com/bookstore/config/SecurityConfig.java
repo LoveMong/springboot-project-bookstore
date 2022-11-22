a72a0f4ee799c39bd5885fd1d00f940bcabb4806
LeeMong <izpzgi2019@gmail.com>
2022-11-22 23:49:42 +0900

    SecurityConfig 주석 수정

--- src/main/java/com/bookstore/config/SecurityConfig.java
+++ src/main/java/com/bookstore/config/SecurityConfig.java
@@ -41,22 +41,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
-                .loginPage("/account/sign-in") // 로그인 페이지 URL을 설정
+                .loginPage("/account/sign-in") // 로그인 페이지 URL 설정
                 .loginProcessingUrl("/login") // form 태그의 action 속성 (default: POST /login)
                 .usernameParameter("memberEmail") // input 태그의 아이디 혹은 이메일 name 속성 값 (default: username)
                 .passwordParameter("memberPassword") // input 태그의 비밀번호 name 속성 값 (default: password)
-                .defaultSuccessUrl("/") // 로그인 성공 시 이동할 URL을 설정
-                .failureUrl("/account/sign-in/error") // 로그인 실패 시 이동할 URL을 설정
+                .defaultSuccessUrl("/") // 로그인 성공 시 이동할 URL 설정
+                .failureUrl("/account/sign-in/error") // 로그인 실패 시 이동할 URL 설정
                 .and()
                 .logout() // 로그아웃 처리
                 .logoutRequestMatcher(new AntPathRequestMatcher("/account/logout")) // 로그아웃 URL 설정
                 .deleteCookies("JSESSIONID", "remember-me") // 로그아웃 후 해당 쿠키 삭제
-                .logoutSuccessUrl("/") // 로그아웃 성공 시 이동할 URL을 설정
+                .logoutSuccessUrl("/") // 로그아웃 성공 시 이동할 URL 설정
         ;
 
 
         http.oauth2Login()
-                .defaultSuccessUrl("/") // 로그인 성공 시 이동할 URL을 설정
+                .defaultSuccessUrl("/") // 로그인 성공 시 이동할 URL 설정
                 .userInfoEndpoint()// 로그인 성공 후 사용자 정보를 가져옴
                 .userService(oauth2MemberService) // 사용자 정보를 처리할 때 사용
         ;
@@ -65,14 +65,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.rememberMe()
                 .tokenValiditySeconds(3600*24*365)
                 .userDetailsService(principalDetailService)
-                .tokenRepository(tokenRepository()) // DB에서 토큰 값 가져오기 (Username, 토큰, 시리즈)
+                .tokenRepository(tokenRepository()) // DB 에서 토큰 값 가져오기 (Username, 토큰, 시리즈)
         ;
 
 
-        http.authorizeRequests() // 시큐리티 처리에 HttpServletRequest를 이용
-                .mvcMatchers("/", "/login", "/logout", "/account/**", "/bookSearchDetail", "/search").permitAll() // 메인, 회원 관련, 상품 관련, 상품 이미지 관련 페이지는 모든 사용자가 로그인(인증)없이 접근 가능
-                .mvcMatchers("/admin/**").hasRole("ADMIN") // admin으로 시작하는 경로는 계정이 ADMIN일 경우에만 접근 가능
-                .anyRequest().authenticated() // 위 mvcMatchers로 설정해준 결로 외 나머지 경로들은 모두 인증 요구
+        http.authorizeRequests() // 시큐리티 처리에 HttpServletRequest 이용
+                .mvcMatchers("/", "/login", "/logout", "/account/**", "/bookSearchDetail", "/search").permitAll()
+                // 메인, 회원 관련, 상품 관련, 상품 이미지 관련 페이지는 모든 사용자가 로그인(인증)없이 접근 가능
+                .mvcMatchers("/admin/**").hasRole("ADMIN") // admin 으로 시작하는 경로는 계정이 ADMIN 일 경우에만 접근 가능
+                .anyRequest().authenticated() // 위 mvcMatchers 로 설정해준 결로 외 나머지 경로들은 모두 인증 요구
         ;
 
 
